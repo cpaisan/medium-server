b006aa1d00b289c93508052ec118649dda01be92
Charlie Paisan <cppaisan@gmail.com>
2019-09-09 07:58:32 -0700

    [FEATURE] Create express server and add endpoint to fetch Medium feeds - This endpoint also updates the search history, which is stored in memory on the server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+const Parser = require("rss-parser");
+
+const parser = new Parser();
+const app = express();
+app.use(cors());
+
+const MEDIUM_URL = "https://www.medium.com/feed";
+let searchHistory = [];
+
+app.get("/medium/:feed_name", async (req, res) => {
+  const {
+    params: { feed_name: feedName = "" }
+  } = req;
+  try {
+    const { items = [] } = await parser.parseURL(`${MEDIUM_URL}/${feedName}`);
+    if (!searchHistory.includes(feedName)) {
+      searchHistory.unshift(feedName);
+    }
+    res.json({ items, searchHistory: searchHistory.slice(0, 5) });
+  } catch (e) {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(3001);
